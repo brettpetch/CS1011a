742233a5976d0ea94b238985728a4c57adf912d6
Brett Petch <bret7600@icloud.com>
2018-12-09 12:01:27 -0500

    Add <noscript> tags.

--- asn3/JSFunctions.js
+++ asn3/JSFunctions.js
@@ -1,3 +1,3 @@
 	function formatNumber(num) {
-		return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-	}
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    }
